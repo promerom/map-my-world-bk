bf62d499047a1878a4d89e9587363e2bd609d127
Plinio Romero <romero.p.mfc@gmail.com>
2024-07-05 15:51:26 -0500

    data modeling
    data modeling definition

---
+++ app/v1/schema/db.py
@@ -0,0 +1,34 @@
+from typing import List
+from uuid import uuid4
+
+from app.v1.model.models import Location, Category
+
+locations: List[Location] = [
+    Location(
+        id=uuid4(),
+        name="Location 1",
+        longitude="1.2.3.4.5.6",
+        latitude="6.5.4.3.2.1"
+    ),
+    Location(
+        id=uuid4(),
+        name="Location 2",
+        longitude="2.3.4.5.6.7",
+        latitude="7.6.5.4.3.2"
+    )
+]
+
+categories: List[Category] = [
+    Category(
+        id=uuid4(),
+        type="park"
+    ),
+    Category(
+        id=uuid4(),
+        type="house"
+    ),
+    Category(
+        id=uuid4(),
+        type="stadium"
+    )
+]

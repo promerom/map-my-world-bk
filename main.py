d2d81b31dceea167327361e99304087beb160844
Plinio Romero <romero.p.mfc@gmail.com>
2024-07-05 23:37:20 -0500

    endpoints create
    defining endpoints firts iteration and update readme

---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+
+from app.v1.model.models import Location, Category
+from app.v1.schema import db
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {"message": "Hello World!"}
+
+
+@app.get("/api/v1/locations")
+def location():
+    return db.locations
+
+
+@app.get("/api/v1/categories")
+def categories():
+    return db.categories
+
+
+@app.post("/api/v1/add/location")
+async def create_location(location: Location):
+    db.locations.append(location)
+    return {"id": location.id}
+
+
+@app.post("/api/v1/add/category")
+async def create_category(category: Category):
+    db.categories.append(category)
+    return {"id": category.id}
+
+
+@app.get("/api/v1/review")
+async def review():
+    return {"review this locations"}
